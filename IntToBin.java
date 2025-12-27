ded7de977b364b47434f232105b006ffce2425c5
Lior Rubin <lior.rubin@post.runi.ac.il>
2025-12-27 15:15:49 +0200

    Finished IntToBin Recursively

--- IntToBin.java
+++ IntToBin.java
@@ -1,14 +1,21 @@
 /** Returns the binary representation of a given integer. */
-public class IntToBin {
-
-    public static void main(String[] args) {
+public class IntToBin 
+{
+    public static void main(String[] args) 
+    {
         int x = Integer.parseInt(args[0]);
         System.out.println("Binary representation of " + x + " is: " + toBinary(x));
     }
 
-    /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
-    public static String toBinary(int x) {
-        //// Replace the following statement with your code
-        return "";
+    /** Returns the binary representation of the given integer, as a string of 0's and 1's.  **/
+    public static String toBinary(int x) 
+    {
+        if (x == 0 || x == 1)
+            return "" + x; // changing the int value into a string
+
+        if (x % 2 == 0) // if even num
+            return toBinary(x/2) + "0";
+
+        return toBinary(x/2) + "1"; // if odd
     }    
- }
+}
