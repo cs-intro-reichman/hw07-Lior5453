e2c7cd662d513435e7cef14e95f515d06dea4f0b
Lior Rubin <lior.rubin@post.runi.ac.il>
2025-12-27 15:22:10 +0200

    finished palindrome recursively

--- Palindrome.java
+++ Palindrome.java
@@ -1,13 +1,20 @@
 /** Checks if a given string is a palindrome. */
-public class Palindrome {
-
-	public static void main(String[]args) {
+public class Palindrome 
+{
+	public static void main(String[]args) 
+	{
     	System.out.println(isPalindrome(args[0]));
     }
 	
 	/** Checks if the given string is a palindrome. */
-	public static boolean isPalindrome(String s) {
-		//// Replace the followaing statement with your code
-		return false;
+	public static boolean isPalindrome(String s) 
+	{
+		if (s.length() <= 1) // if cuccrent length is 1 or lower return true (all checked or string is automaticly a palindrome if length 0/1)
+			return true;
+
+		if (s.charAt(0) != s.charAt(s.length() - 1)) // if letters are different in corners return false
+			return false;
+
+		return isPalindrome(s.substring(1, s.length() - 1)); // removing both corners and moving on to keep checking
     }
 }
